fd1309de7c116ca771bfa80a1e3d743cd1110fbe
Nahiyan Kamal <noyanayihan@gmail.com>
2020-12-04 01:38:05 +0100

    fix: found critical case

--- test/console-table-printer/tryTable.test.ts
+++ test/console-table-printer/tryTable.test.ts
@@ -8,8 +8,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'expired',
         reason: 'fraud',
         currency: 'GBP',
-        bookign_session_id_1: 'e01938b3-4651-451b-8b41-e86d3804b83a',
-        bookign_session_id: '1b8677ca-cd65-4a0d-82c0-7b3fdfa1f0c3',
         amount_money: 4392.58,
       },
       {
@@ -17,8 +15,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'expired',
         reason: 'fraud',
         currency: 'CAD',
-        bookign_session_id_1: '9d64cc51-5b5f-4a87-bc6b-a60dfe214021',
-        bookign_session_id: '9d64cc51-5b5f-4a87-bc6b-a60dfe214021',
         amount_money: 1462.25,
       },
       {
@@ -26,8 +22,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'expired',
         reason: 'fraud',
         currency: 'USD',
-        bookign_session_id_1: 'f9ce568b-c1b5-4bfd-9cbf-bf63a68650cb',
-        bookign_session_id: '31e3a9f7-14d8-4211-93b5-25b18f1882c7',
         amount_money: 5622.29,
       },
       {
@@ -35,8 +29,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'open',
         reason: 'fraud',
         currency: 'EUR',
-        bookign_session_id_1: 'd392d634-17e8-4892-93f6-ccb1952f8f69',
-        bookign_session_id: 'd392d634-17e8-4892-93f6-ccb1952f8f69',
         amount_money: 1037.98,
       },
       {
@@ -44,8 +36,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'disputed',
         reason: 'fraud',
         currency: 'GBP',
-        bookign_session_id_1: 'ed37bfed-46cf-452d-8995-12f9987649b2',
-        bookign_session_id: 'ed37bfed-46cf-452d-8995-12f9987649b2',
         amount_money: 825.16,
       },
       {
@@ -53,8 +43,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'lost',
         reason: 'fraud',
         currency: 'EUR',
-        bookign_session_id_1: 'f8e15b3e-f661-40cf-9651-4139990fca36',
-        bookign_session_id: '4022d124-7546-42fc-a242-767f4bc1fbe5',
         amount_money: 8273,
       },
       {
@@ -62,8 +50,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'disputed',
         reason: 'fraud',
         currency: 'DKK',
-        bookign_session_id_1: 'f380d0b3-66bb-4827-a111-594fabe40b51',
-        bookign_session_id: 'f380d0b3-66bb-4827-a111-594fabe40b51',
         amount_money: 976.47,
       },
       {
@@ -71,8 +57,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'disputed',
         reason: 'fraud',
         currency: 'EUR',
-        bookign_session_id_1: 'cbc5351b-6ed0-49ef-bc7c-84b40b5b9949',
-        bookign_session_id: '20fe2889-6df0-4e3d-80f8-7c504ce65de2',
         amount_money: 11118.2,
       },
       {
@@ -80,8 +64,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'won',
         reason: 'fraud',
         currency: 'EUR',
-        bookign_session_id_1: 'cbc5351b-6ed0-49ef-bc7c-84b40b5b9949',
-        bookign_session_id: '2ead3e50-96fa-43ab-85c0-195497cba0ef',
         amount_money: 9340,
       },
       {
@@ -89,8 +71,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'expired',
         reason: 'fraud',
         currency: 'DKK',
-        bookign_session_id_1: 'b52fb3b9-6c1f-44ea-852a-4659688bed37',
-        bookign_session_id: '1dac0fda-ec07-4e3a-a076-12b9175ff910',
         amount_money: 8178.51,
       },
       {
@@ -98,8 +78,6 @@ describe('Example: Print a simple Table without table instance creation', () => 
         status: 'expired',
         reason: 'fraud',
         currency: 'EUR',
-        bookign_session_id_1: 'f20d317d-56c5-4467-a5bf-89399663391a',
-        bookign_session_id: '0492b0a8-9479-47a5-b5dc-37ea5b7c7108',
         amount_money: 46253.72,
       },
     ]);
