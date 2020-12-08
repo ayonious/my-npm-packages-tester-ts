ca98c877e0b10e4addbaecfdc2b93e70032bac0a
Nahiyan Kamal <noyanayihan@gmail.com>
2020-12-08 10:30:56 +0100

    fix(technical): upodate deps

--- test/simple-wcswidth/tryRules.test.ts
+++ test/simple-wcswidth/tryRules.test.ts
@@ -3,6 +3,8 @@ import { wcswidth } from 'simple-wcswidth';
 describe('Example: Simple', () => {
   it(`Simple`, function () {
     // Step1: Define your conditional rules
-    expect(wcswidth('asdf')).toBe(4);
+    expect(wcswidth('Yes 重要')).toBe(8);
+    expect(wcswidth('请你')).toBe(4);
+    expect(wcswidth('Hi')).toBe(2);
   });
 });
