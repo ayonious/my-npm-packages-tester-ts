0564c8c1682b7e39b587a9c313d8aa659e7207d2
Nahiyan Kamal <noyanayihan@gmail.com>
2022-04-03 14:12:26 +0200

    chore: new exam[le

---
+++ src/console-table-printer/test6: Readme6/tryTable1.ts
@@ -0,0 +1,26 @@
+import { Table } from 'console-table-printer';
+
+const p = new Table({
+  columns: [
+    { name: 'index', alignment: 'left', color: 'blue' }, // with alignment and color
+    { name: 'text', alignment: 'right' },
+  ],
+  colorMap: {
+    custom_green: '\x1b[32m', // define customized color
+  },
+});
+
+p.addRow({ index: 1, text: 'red wine' }, { color: 'green' });
+p.addRow(
+  { index: 2, text: 'green gemuse'},
+  { color: 'custom_green' } // your green
+);
+p.addRow(
+  { index: 3, text: 'gelb bananen'},
+  { color: 'yellow' }
+);
+p.addRow(
+  { index: 3, text: 'rosa hemd wie immer'},
+  { color: 'cyan' }
+);
+p.printTable();
