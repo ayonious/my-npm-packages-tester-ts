b0d85b49f28e9b380137cb4eb8bbcde0bf591a60
Nahiyan Kamal <noyanayihan@gmail.com>
2022-04-03 13:53:57 +0200

    chore: update deps

--- src/console-table-printer/test5: Readme5/tryTable1.ts
+++ src/console-table-printer/test5: Readme5/tryTable1.ts
@@ -2,13 +2,20 @@ import { Table } from 'console-table-printer';
 
 const p = new Table({
   columns: [
-    { name: 'index', alignment: 'left', color: 'green' }, //with alignment and color
+    { name: 'index', alignment: 'left', color: 'blue' }, // with alignment and color
     { name: 'text', alignment: 'right' },
+    { name: 'is_priority_today', title: 'Is This Priority?' }, // with Title as separate Text
   ],
+  colorMap: {
+    custom_green: '\x1b[32m', // define customized color
+  },
 });
 
-p.addRow({ index: 1, text: 'red wine', value: 10.212 }, { color: 'red' });
-p.addRow({ index: 2, text: 'green gemuse', value: 20.0 });
+p.addRow({ index: 1, text: 'red wine', value: 10.212 }, { color: 'green' });
+p.addRow(
+  { index: 2, text: 'green gemuse', value: 20.0 },
+  { color: 'custom_green' } // your green
+);
 p.addRow(
   { index: 3, text: 'gelb bananen', value: 100, is_priority_today: 'Y' },
   { color: 'yellow' }
