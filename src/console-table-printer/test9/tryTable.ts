5ac3e0ce3d28c0e6d8bfd7d0c228bdb378ee1009
Nahiyan Kamal <noyanayihan@gmail.com>
2024-06-20 09:10:31 +0200

    test message

---
+++ src/console-table-printer/test9/tryTable.ts
@@ -0,0 +1,86 @@
+import { Table } from 'console-table-printer';
+
+const table = new Table();
+
+table.addRows([
+  {
+    _count: 3,
+    status: 'expired',
+    reason: 'fraud',
+    currency: 'GBP',
+    amount_money: 4392.58,
+  },
+  {
+    _count: 1,
+    status: 'expired',
+    reason: 'fraud',
+    currency: 'CAD',
+    amount_money: 1462.25,
+  },
+  {
+    _count: 4,
+    status: 'expired',
+    reason: 'fraud',
+    currency: 'USD',
+    amount_money: 5622.29,
+  },
+  {
+    _count: 1,
+    status: 'open',
+    reason: 'fraud',
+    currency: 'EUR',
+    amount_money: 1037.98,
+  },
+  {
+    _count: 1,
+    status: 'disputed',
+    reason: 'fraud',
+    currency: 'GBP',
+    amount_money: 825.16,
+  },
+  {
+    _count: 7,
+    status: 'lost',
+    reason: 'fraud',
+    currency: 'EUR',
+    amount_money: 8273,
+  },
+  {
+    _count: 1,
+    status: 'disputed',
+    reason: 'fraud',
+    currency: 'DKK',
+    amount_money: 976.47,
+  },
+  {
+    _count: 12,
+    status: 'disputed',
+    reason: 'fraud',
+    currency: 'EUR',
+    amount_money: 11118.2,
+  },
+  {
+    _count: 10,
+    status: 'won',
+    reason: 'fraud',
+    currency: 'EUR',
+    amount_money: 9340,
+  },
+  {
+    _count: 5,
+    status: 'expired',
+    reason: 'fraud',
+    currency: 'DKK',
+    amount_money: 8178.51,
+  },
+  {
+    _count: 33,
+    status: 'expired',
+    reason: 'fraud',
+    currency: 'EUR',
+    amount_money: 46253.72,
+  },
+]);
+
+console.log(table.render());
+// newTable;
