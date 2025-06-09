c030718c9b370f3160de14cd17940e866529efbe
Nahiyan Kamal <noyanayihan@gmail.com>
2025-06-09 22:45:04 +0200

    chore: upate deps

---
+++ src/console-table-printer/try12:defaultColStyle/example1.ts
@@ -0,0 +1,28 @@
+import { printTable, Table } from "console-table-printer";
+
+const data = [
+  { __name__: "John", age: 20 },
+  { __name__: "Jane", age: 21 },
+  { __name__: "Jim", age: 2288989898 },
+];
+
+printTable(data, {
+  defaultColumnOptions: {
+    alignment: "left",
+    color: "red",
+  },
+});
+
+const table = new Table({
+  columns: [
+    { name: "__name__" },
+    { name: "age" },
+  ],
+  defaultColumnOptions: {
+    alignment: "center",
+    color: "red",
+  },
+});
+
+table.addRows(data);
+table.printTable();
